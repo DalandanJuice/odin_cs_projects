a1fb5e343be3c1a73280f2e254357a8fe8dcaf20
Mark Genesis Sargento <37236889+DalandanJuice@users.noreply.github.com>
2020-03-30 13:48:15 +0800

    Fix function name

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -1,11 +1,11 @@
 def merge_sort(arr)
   return arr if arr.length <= 1
-  a = alternative_merge(arr[0..(arr.length - 1)/2])
-  b = alternative_merge(arr[(arr.length - 1)/2 + 1..arr.length - 1])
+  a = merge_sort(arr[0..(arr.length - 1)/2])
+  b = merge_sort(arr[(arr.length - 1)/2 + 1..arr.length - 1])
   if a.first < b.first
     merge = [a.shift]
-    return merge + alternative_merge(a + b)
+    return merge + merge_sort(a + b)
   end
     merge = [b.shift]
-    return merge + alternative_merge(b + a)
+    return merge + merge_sort(b + a)
 end
